2832fe4de2c3d29880d43e0b89685c2570417cdd
Michael Pope <michael@Michaels-MBP.domain_not_set.invalid>
2023-07-07 16:24:33 +0200

    Feedbackpage now pulls from json-server

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -11,24 +11,25 @@ export default function Feedback() {
   }, []);
 
   const fetchData = async () => {
-    const res = await fetch(
-      "http://127.0.0.1:8090/api/collections/Feedback/records?page=1&perPage=20"
-    );
+    const res = await fetch("http://localhost:4000/answers");
     const data = await res.json();
-    setData(data.items);
+    console.log(data);
+    setData(data);
   };
+
+  // TODO: Look into improving nameing: entry.data.data.map is not very readable
 
   return (
     <main>
       <h2>Feedback</h2>
       <div id="FeedbackContainer">
         {!data ? (
-          <div>Loading</div>
+          <div>Loading...</div>
         ) : (
           data.map((entry: any, i: number) => (
             <Card key={i}>
               <div className="qAndA">
-                {entry.data.map((question: any, i: number) => (
+                {entry.data.data.map((question: any, i: number) => (
                   <div key={i} className="pairs">
                     <h3>{question.question}</h3>
                     <p>{question.value}</p>
